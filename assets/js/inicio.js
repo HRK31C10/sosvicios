273eeec94a9a9ddb5bce7e234157eaa33b384fec
HERICK CEZAR DIAS ANDRADE <herick.cdandrade@senacsp.edu.br>
2022-09-30 10:33:41 -0300

    first commit

---
+++ assets/js/inicio.js
@@ -0,0 +1,22 @@
+function mouseOver () {
+    document.getElementById("tratamentos1").classList.add('tratamentos1')
+}
+
+function mouseOut () {
+    document.getElementById("tratamentos1").classList.add('tratamentosog');
+}
+function mouseOver2 () {
+    document.getElementById("tratamentos2").classList.add('tratamentos1')
+}
+
+function mouseOut2 () {
+    document.getElementById("tratamentos2").classList.add('tratamentosog');
+}
+function mouseOver3 () {
+    document.getElementById("tratamentos3").classList.add('tratamentos1')
+}
+
+function mouseOut3 () {
+    document.getElementById("tratamentos3").classList.add('tratamentosog');
+}
+
